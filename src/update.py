ce157ff6f1e1ad6454421024ba1db1d06c786fff
Nanne Baars <nanne.baars@ing.com>
2020-11-24 17:58:05 +0100

    Fix formatting issues

--- src/update.py
+++ src/update.py
@@ -2,12 +2,14 @@
 
 from workflow import web, Workflow, PasswordNotFound
 
+
 def get_projects(api_key, url):
     """
     Parse all pages of projects
     :return: list
     """
     return get_project_page(api_key, url, 1, [])
+
 
 def get_project_page(api_key, url, page, list):
     log.info("Calling API page {page}".format(page=page))
@@ -28,25 +30,27 @@ def get_project_page(api_key, url, page, list):
 
     return result
 
-def main(wf):
+
+def main(workflow):
     try:
         # Get API key from Keychain
-        api_key = wf.get_password('gitlab_api_key')
-        api_url = wf.settings.get('api_url')
+        api_key = workflow.get_password('gitlab_api_key')
+        api_url = workflow.settings.get('api_url')
 
         # Retrieve projects from cache if available and no more than 600
         # seconds old
         def wrapper():
             return get_projects(api_key, api_url)
 
-        projects = wf.cached_data('projects', wrapper, max_age=3600)
+        projects = workflow.cached_data('projects', wrapper, max_age=3600)
 
         # Record our progress in the log file
         log.debug('{} gitlab projects cached'.format(len(projects)))
 
     except PasswordNotFound:  # API key has not yet been set
         # Nothing we can do about this, so just log it
-        wf.logger.error('No API key saved')
+        workflow.logger.error('No API key saved')
+
 
 if __name__ == u"__main__":
     wf = Workflow()
